6ac30dc3e46643038bb5605954acf4c10ab816a8
Mark Pittaway <mark.pittaway@gmail.com>
2018-08-23 16:22:13 +1000

    fix(typescript): Update configs to get typescript working in tests

--- index.js
+++ index.js
@@ -4,20 +4,4 @@ import './client/styles/analytics.scss';
 // scripts
 import analyticsModule from './client';
 
-// configureAnalytics.$inject = ['superdeskProvider']
-// function configureAnalytics(superdesk) {
-//     superdesk
-//         .activity('/analytics', {
-//             label: gettext('Analytics'),
-//             description: gettext('Analytics'),
-//             when: '/analytics',
-//             templateUrl: './client/views/analytics.html',
-//             topTemplateUrl: 'scripts/apps/dashboard/views/workspace-topnav.html',
-//             sideTemplateUrl: 'scripts/apps/workspace/views/workspace-sidenav.html',
-//             category: 'analytics',
-//             priority: -800
-//         })
-// }
-
-// export default analyticsModule.config(configureAnalytics)
 export default analyticsModule;
