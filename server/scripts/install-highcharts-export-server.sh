a91bdb76a4b03113835cbbaaeb03278bb97a1d8f
MarkLark86 <mark.pittaway@gmail.com>
2020-01-29 15:39:27 +1100

    fix(install): Fix installing mkdir for highcharts-export-server (#115)

--- server/scripts/install-highcharts-export-server.sh
+++ server/scripts/install-highcharts-export-server.sh
@@ -6,3 +6,10 @@ export HIGHCHARTS_USE_STYLED=1
 export HIGHCHARTS_MOMENT=1
 
 npm install -g highcharts-export-server
+
+# Hack to force mkdirp version
+# until https://github.com/highcharts/node-export-server/pull/231 is merged
+MODULES_DIR=`npm root -g`
+npm install -g mkdirp@0.5.1
+rm -Rf $MODULES_DIR/highcharts-export-server/node_modules/mkdirp
+ln -s $MODULES_DIR/mkdirp $MODULES_DIR/highcharts-export-server/node_modules/mkdirp
