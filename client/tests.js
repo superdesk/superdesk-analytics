0f47489666d69e613661c7ede72757048ffb841e
Mark Pittaway <mark.pittaway@gmail.com>
2020-06-04 11:27:13 +1000

    fix: Use release/1.33.1 for superdesk-core

--- client/tests.js
+++ client/tests.js
@@ -7,8 +7,6 @@ import {debugInfo} from 'appConfig';
 
 import './';
 
-debugInfo.translationsLoaded = true; // don't print warnings about missing translations when running unit tests
-
 var testsContext = require.context('.', true, /.spec.[j|t]sx?$/);
 
 testsContext.keys().forEach(testsContext);
