68ab35f39bfa4d91ac3b7455a5eabb00cd9c0fc4
Mark Pittaway <mark.pittaway@gmail.com>
2022-06-01 14:46:53 +1000

    [SDESK-6490] fix: Use Highcharts cli instead of the HTTP server This removes the need for the Highcharts HTTP server and uses the cli instead. Only used when emailing reports that contain reports/charts as images

--- server/scripts/install-highcharts-export-server.sh
+++ server/scripts/install-highcharts-export-server.sh
@@ -5,7 +5,4 @@ export HIGHCHARTS_VERSION=6.2.0
 export HIGHCHARTS_USE_STYLED=1
 export HIGHCHARTS_MOMENT=1
 
-# Hack to force using a newer commit form github
-# due to issues with uui module version requirements
-npm install -g highcharts/node-export-server#e3bd98c
-# npm install -g highcharts-export-server
+npm install -g highcharts-export-server@2.1
