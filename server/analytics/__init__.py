63a8ac37d71125073da3bd50b1478a46b49d08c4
Mark Pittaway <mark.pittaway@gmail.com>
2022-06-14 18:02:29 +1000

    remove unused path import

--- server/analytics/__init__.py
+++ server/analytics/__init__.py
@@ -8,7 +8,6 @@
 # AUTHORS and LICENSE files distributed with this source code, or
 # at https://www.sourcefabric.org/superdesk/license
 
-from os import path
 import superdesk
 
 from analytics.report_configs import ReportConfigsResource, ReportConfigsService
