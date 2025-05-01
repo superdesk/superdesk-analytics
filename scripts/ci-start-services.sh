f17f489b3e774ae80f78d84799ef6a22df59de69
Helmy Giacoman <helmygb@gmail.com>
2025-05-01 22:54:49 +0200

    Fix docker compose command

--- scripts/ci-start-services.sh
+++ scripts/ci-start-services.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 if [ "$SERVER" == "true" ]; then
-    docker-compose -f .travis-docker-compose.yml up -d
+    docker compose -f .travis-docker-compose.yml up -d
     while ! curl -sfo /dev/null 'http://localhost:9200/'; do echo -n '.' && sleep .5; done
 fi
