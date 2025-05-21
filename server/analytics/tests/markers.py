3a879a6ce0856c5514976e72a021738a4fc49747
Helmy Giacoman <helmygb@gmail.com>
2025-05-21 14:41:03 +0200

    Fix pytests connected with commands and reports
    SDESK-7315

--- server/analytics/tests/markers.py
+++
@@ -1,8 +0,0 @@
-from pytest import mark
-
-# Skips the test, due to commands not being async
-requires_async_commands = mark.skip(reason="Requires commands to be async")
-
-# highcharts server generation is not working on Github Actions
-# need to figure out why and then enable the tests
-fix_highchart_server = mark.skip(reason="Highcharts server fails")
