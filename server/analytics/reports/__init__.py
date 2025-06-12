78af1ab5a4782f30d27a512d20edc62986566a37
Helmy Giacoman <helmygb@gmail.com>
2025-06-12 14:22:22 +0200

    Increase timeout to give more time to complete

--- server/analytics/reports/__init__.py
+++ server/analytics/reports/__init__.py
@@ -165,7 +165,7 @@ def _run_highcharts_cli(in_file: str, out_file: str, mimetype: str, width: Optio
             text=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
-            timeout=5,  # Don't allow process to run for more than 5 seconds,
+            timeout=10,  # Don't allow process to run for more than 10 seconds,
         )
 
         if not path.exists(out_file):
