bb4242804e7005ec20da86522ddc395a2fba288c
Mark Pittaway <mark.pittaway@gmail.com>
2025-09-01 15:03:40 +1000

    Release 3.0.0-beta.1

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ package_data = {
 
 setup(
     name="superdesk-analytics",
-    version="3.0.0-dev.0",
+    version="3.0.0-beta.1",
     package_dir={"": "server"},
     packages=find_packages("server"),
     package_data=package_data,
