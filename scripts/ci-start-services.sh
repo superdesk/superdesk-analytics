3d86aa1f2618b79773c68309617c4c73eb907eae
Helmy Giacoman <helmygb@gmail.com>
2025-12-02 12:27:26 +0100

    Clean up CI stuff

--- scripts/ci-start-services.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$SERVER" == "true" ]; then
-    docker compose -f .travis-docker-compose.yml up -d
-    while ! curl -sfo /dev/null 'http://localhost:9200/'; do echo -n '.' && sleep .5; done
-fi
